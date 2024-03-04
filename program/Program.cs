7750974154b6c8429f82ba3b8d1be3a0b86fffee
DaviesNyantakyi <davies.nyantakyi@outlook.col>
2024-03-04 18:37:56 +0100

    first commit

---
+++ program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+
+
+// Create an array of integers and write a program to find the sum of all even numbers in the array.
+class MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+            // create a varbiable sum;
+            // Loop trough the numbers
+            // Check if the remainder of the number is 0
+            // add to sum if the remainder is 0.
+
+            int sum = 0;
+
+            for(int i = 0; i< numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+            
+    }
+}
+
